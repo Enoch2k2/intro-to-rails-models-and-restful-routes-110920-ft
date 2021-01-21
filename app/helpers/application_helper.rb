46234aec01a4347068093069119dd67f6a65bc6d
Enoch2k2 <enoch2k2@hotmail.com>
2021-01-21 13:48:08 -0600

    added helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+  def render_nav_links
+    content_tag("ul", class: "right") do
+      if false # is logged in
+          content_tag("li") do
+            link_to("Movies", movies_path)
+          end + 
+          content_tag("li") do
+            link_to("Create Movie", new_movie_path)
+          end
+      else
+        content_tag("li") do
+          link_to("Login", "#")
+        end + 
+        content_tag("li") do
+          link_to("Signup", "#")
+        end
+      end
+    end
+  end
 end
--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,2 +1,9 @@
 module MoviesHelper
+  def genre_name(movie)
+    if movie.genre
+      movie.genre.name
+    else
+      link_to "Add Genre", edit_movie_path(movie)
+    end
+  end
 end
